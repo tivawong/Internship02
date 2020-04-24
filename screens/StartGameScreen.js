350d5e21a2281670b3e4abcab9d8ee5775d06d97
tivaiice <tiva.wk@hotmail.com>
2020-04-24 10:44:52 +0700

    commit 3

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Text, StyleSheet, View, TextInput, Button } from 'react-native'
 
 import Card from '../components/Card';
-import Input from '../components/Input';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('')
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
     return (
         <View style={{flex:1,padding:10,alignItems:'center'}}>
             <Text style={{fontSize: 20, marginVertical: 10}}> The Game Screen </Text>
                 <Card style={{alignItems:'center',shadowColor:'red',shadowOffset: {width:0, height:2}, shadowRadius:6, shadowOpacity:0.26, backgroundColor: 'white' ,elevation:5, padding:20, borderRadius: 10}}>
                     <Text>Select A Number !</Text>
-                    <Input 
+                    <TextInput 
                         style={{height:30,width: 100, borderBottomColor: 'grey', borderWidth:1, marginVertical:10}} 
                         blurOnSumbmit 
                         autoCapitalize='none' 
-                        autoCapitalize={false} 
-                        keyboardType="numeric"
+                        autoCorrect={false} 
+                        keyboardType="number-pad"
                         maxLength={2}
+                        onChangeTex={numberInputHandler}
+                        value={enteredValue}
                     />
 
                     <View style={{flexDirection:'row',justifyContent:'space-between',paddingHorizontal:15}}>
